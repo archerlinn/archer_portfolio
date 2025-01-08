bc4d7415c2cdcbfa801bac84f6926cca1f2abf30
archerlinn <archerlin0530@gmail.com>
2025-01-08 21:22:28 +0800

    updates

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ contactForm.addEventListener('submit', (e) => {
     return;
   }
 
-  fetch('https://archer-portfolio.onrender.com/send-email', {
+  fetch('https://www.archerlin.space/send-email', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -77,7 +77,7 @@ function sendMessage() {
   addMessageToChat(userMsg, "user-msg");
   userInput.value = "";
 
-  fetch('https://archer-portfolio.onrender.com/chat', {
+  fetch('https://www.archerlin.space/chat', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
